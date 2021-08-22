4f6175f2e025babf2fb748248bd43fadf3ec99c2
Malytherin <35782511+Malytherin@users.noreply.github.com>
2021-08-22 13:35:59 -0400

    Update PracticeCode_DELETE.R
    Removed v3 (not needed)

--- PracticeCode_DELETE.R
+++ PracticeCode_DELETE.R
@@ -10,4 +10,3 @@ colnames(df) <- c("Group","Age")
 df
 Plot <- ggplot(df, aes(x=Group, y = Age, color = Group)) + geom_boxplot() + geom_jitter(shape=16, size= 3, position=position_jitter(0.2)) + stat_summary(fun=mean, geom="point", shape=23, size=5)
 PlotPlot
-v3 <- c(2,5,8,2,7,8,1,4,5,5,7)
